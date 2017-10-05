69890118eb1f35f30dbaedcb0899e510e83b5eac
Conrado Costa <mcostaconrado@gmail.com>
2017-10-05 16:24:50 -0300

    ghf

--- main.lua
+++ main.lua
@@ -23,14 +23,6 @@ end
 function love.update(dt)
 	cursor.x = love.mouse.getX()
 	cursor.y = love.mouse.getY()
-	
-	if (isCircle) then
-		numCoord = numCoord + 1;
-		coordX[numCoord] = cursor.x;
-   		coordY[numCoord] = cursor.y;
-   		isCircle = false
-   	end
-	print(numCoord)
 end
 
 
@@ -41,15 +33,23 @@ function love.draw()
 	love.graphics.setColor(255,0,0)
 	love.graphics.circle("fill", cursor.x, cursor.y, 15, 100)
 	
+  if (isCircle) then
+		numCoord = numCoord + 1;
+		coordX[numCoord] = cursor.x;
+   		coordY[numCoord] = cursor.y;
+   		isCircle = false
+   	end
+	print(numCoord)
+  
 	for i=1, numCoord do
 		love.graphics.circle("fill", coordX[i], coordY[i], 15, 100)
 	end
    	
 end
 
-function love.mouse.isDown(x, y, button, istouch)
+function love.mousepressed(x, y, button)
    if button == 1 then -- Versions prior to 0.10.0 use the MouseConstant 'l'
-   		isCircle = true
+    isCircle = true
 		--love.graphics.setColor(255, 0,0);
 		--love.graphics.circle("fill", x, y, 50, 1000)
    end
