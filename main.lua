2d2540cb63172f3b69cce129a23f0930022ef33f
mcostaconrado <mcostaconrado@gmail.com>
2017-10-05 18:42:47 -0300

    parametros no .txt

--- main.lua
+++ main.lua
@@ -1,9 +1,8 @@
 cursor={
+
 	x=0,
 	y=0
 }
-
-radius = 5
 
 coordX = {}
 coordY = {}
@@ -13,12 +12,40 @@ for i=1, 10000 do
 	coordY[i] = 0
 end
 
-numCoord = 0
+HD = {
+	radius;
+	numPoints = 0;
+}
 
+strebelle = {
+	image,
+	size={
+		x,
+		y
+	};
+}
 
 function love.load()
-	strebelle = love.graphics.newImage("images/Strebelle_B&W_250x250.bmp")
-	love.window.setMode(250, 250)
+
+	data = love.filesystem.newFile("data.txt")
+	data:open("w")
+	
+	local i = 1
+	local parameters = {}
+	
+	for word in love.filesystem.lines("file.txt", " ") do
+		parameters[i] = word
+		i = i + 1
+	end
+	
+	strebelle.size.x = parameters[1]
+	strebelle.size.y = parameters[2]
+	HD.radius = parameters[3]
+	
+	strebelle.image = love.graphics.newImage("images/Strebelle_B&W_" .. strebelle.size.x .. "x" .. strebelle.size.y .. ".bmp")
+	love.window.setMode(strebelle.size.x, strebelle.size.y)
+	
+	data:close()
 end
 
 
@@ -27,33 +54,28 @@ function love.update(dt)
 	cursor.y = love.mouse.getY()
 	
 	if (isCircle) then
-		numCoord = numCoord + 1;
-		coordX[numCoord] = cursor.x;
-   		coordY[numCoord] = cursor.y;
+		coordX[HD.numPoints] = cursor.x;
+   		coordY[HD.numPoints] = cursor.y;
    		isCircle = false
    	end
-	print(numCoord)
 end
 
 
 function love.draw()
 	love.graphics.setColor(255,255,255) 
-	love.graphics.draw(strebelle, 0,0)
+	love.graphics.draw(strebelle.image, 0,0)
 	
 	love.graphics.setColor(255,0,0)
-	love.graphics.circle("fill", cursor.x, cursor.y, radius, 100)
+	love.graphics.circle("fill", cursor.x, cursor.y, HD.radius, 100)
 	
-	for i=1, numCoord do
-		love.graphics.circle("fill", coordX[i], coordY[i], radius, 100)
-	end	
-   	
+	for i=1, HD.numPoints do
+		love.graphics.circle("fill", coordX[i], coordY[i], HD.radius, 100)
+	end	   	
 end
 
-function love.mousepressed(x, y, button)
-   if button == "l" or button == 1 then -- Versions prior to 0.10.0 use the MouseConstant 'l'
+function love.mousereleased(x, y, button)
+	if button == "l" or button == 1 then -- Versions prior to 0.10.0 use the MouseConstant 'l'
    		isCircle = true
-   		numCoord = numCoord + 1
-		--love.graphics.setColor(255, 0,0);
-		--love.graphics.circle("fill", x, y, 50, 1000)
+   		HD.numPoints = HD.numPoints + 1
    end
 end
